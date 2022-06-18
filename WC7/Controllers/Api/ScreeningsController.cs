3aac88b3b63326017f1b3ad639cb7864136ff3f4
saxomoose <mathieu.developer@protonmail.com>
2022-06-18 19:22:13 +0200

    WC7

---
+++ WC7/Controllers/Api/ScreeningsController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WC7.Data;
+using WC7.Models;
+
+namespace WC7.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ScreeningsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ScreeningsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Screenings
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Screening>>> GetScreenings()
+        {
+            return await _context.Screenings.ToListAsync();
+        }
+
+        // GET: api/Screenings/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Screening>> GetScreening(int id)
+        {
+            var screening = await _context.Screenings.FindAsync(id);
+
+            if (screening == null)
+            {
+                return NotFound();
+            }
+
+            return screening;
+        }
+
+        // PUT: api/Screenings/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutScreening(int id, Screening screening)
+        {
+            if (id != screening.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(screening).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ScreeningExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Screenings
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Screening>> PostScreening(Screening screening)
+        {
+            _context.Screenings.Add(screening);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetScreening", new { id = screening.Id }, screening);
+        }
+
+        // DELETE: api/Screenings/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Screening>> DeleteScreening(int id)
+        {
+            var screening = await _context.Screenings.FindAsync(id);
+            if (screening == null)
+            {
+                return NotFound();
+            }
+
+            _context.Screenings.Remove(screening);
+            await _context.SaveChangesAsync();
+
+            return screening;
+        }
+
+        private bool ScreeningExists(int id)
+        {
+            return _context.Screenings.Any(e => e.Id == id);
+        }
+    }
+}
